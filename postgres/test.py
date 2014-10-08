de9f655f63984c62c00a6c41bb2dc380eb8e0d6f
snaiffer <snaiffer@gmail.com>
2014-10-09 02:49:54 +0400

    some tests with psycopg2

--- postgres/test.py
+++ postgres/test.py
@@ -3,30 +3,38 @@ import psycopg2
 import psycopg2.extras
 
 try:
-  conn=psycopg2.connect("dbname='testdb2' user='postgres' password='123456'")
+  with psycopg2.connect("dbname='testdb2' user='postgres' password='123456'") as conn:
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+      try:
+        cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+        conn.commit()
+      except psycopg2.Error:
+        conn.rollback()
+        pass
+
+      try:
+        num = int(input("Enter 'num': "))
+        data = str(input("Enter 'data': "))
+        SQL = "INSERT INTO test (num, data) VALUES (%s, %s)"
+        data = (num, data)
+        cur.execute( SQL % data )
+        conn.commit()
+      except psycopg2.Error as e:
+        conn.rollback()
+        print("Error: Couldn't execute the query!")
+        print(str(e.pgerror))
+
+      try:
+        cur.execute("SELECT * from test")
+        conn.commit()
+      except psycopg2.Error:
+        conn.rollback()
+        print("Error: Couldn't execute the query!")
+      else:
+        rows = cur.fetchall()
+        print("\nRows: \n")
+        for row in rows:
+          print( " " + str(row['num']) + "\t" + str(row['data']))
+
 except psycopg2.Error:
-  print("Error: Could not connect to the database!")
-else:
-  cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-  try:
-    cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-  except psycopg2.Error:
-    pass
-  cur.close()
-  conn.close()
-
-try:
-  conn=psycopg2.connect("dbname='testdb2' user='postgres' password='123456'")
-except psycopg2.Error:
-  print("Error: Could not connect to the database!")
-else:
-  cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-  try:
-    cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
-    conn.commit()
-  except psycopg2.Error:
-    print("Error: Couldn't execute the query!")
-
-  cur.close()
-  conn.close()
+  print("Error: During working with the database!")
