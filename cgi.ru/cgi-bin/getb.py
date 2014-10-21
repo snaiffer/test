3642025e31d00b665b8a2432350a8ac7f9c62eee
snaiffer <snaiffer@gmail.com>
2014-10-21 20:18:58 +0400

    Output via jstree with ajax works

--- cgi.ru/cgi-bin/getb.py
+++ cgi.ru/cgi-bin/getb.py
@@ -5,9 +5,9 @@ treename = "treemind2" #
 import general
 import forest as f
 import tree as t
+import branch as b
 
 import json
-
 
 def for_test():
   try:
@@ -20,6 +20,7 @@ def for_test():
   try:
     with t.Tree(treename) as tree:
       b1 = tree.insertB({'parent_id' : general.rootB_id, 'text' : 'branch1', 'folded' : True})
+      #b1 = tree.insertB({'parent_id' : general.rootB_id, 'text' : 'branch1'})
       b2 = tree.insertB({'parent_id' : general.rootB_id, 'text' : 'branch2'})
       b11 = tree.insertB({'parent_id' : b1.id, 'text' : 'branch11'})
       b12 = tree.insertB({'parent_id' : b1.id, 'text' : 'branch12'})
@@ -32,10 +33,13 @@ for_test()
 
 form = cgi.FieldStorage()
 id = form.getvalue('id')
-if id == None:
-  id = general.rootB_id
 
 print("Content-Type: text/html\n") 
+
+#logfile = open('log', 'w')
+#print('', file=logfile)
+#print(str(id), file=logfile)
+
 
 def getDict(branch):
   dict = {}
@@ -52,12 +56,22 @@ def getDict(branch):
       dict['children'] = True
   return dict  
 
+def getList_subbsOf(branch):
+  list = []
+  if branch.subbs_id != [] :
+    for cur_subb in branch.subbs:
+      list.append(getDict(cur_subb))
+  return list    
+
 try:
   with t.Tree(treename) as tree:
-    print(json.dumps(getDict(tree.getB(id))))
-except BranchException:  
+    if id == None or id == general.rootB_id:
+      print(json.dumps(getDict(tree.getB(general.rootB_id))))
+    else:  
+      print(json.dumps(getList_subbsOf(tree.getB(id))))
+except b.BranchException:  
   print("Error: BranchException has occured")
-except tree.TreeException:  
+except t.TreeException:  
   print("Error: TreeException has occured")
 
 
@@ -73,4 +87,5 @@ print("\
 ")
 """
 
+#logfile.close()
 
