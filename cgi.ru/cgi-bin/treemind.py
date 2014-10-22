ecf06626715ac1eb4509d09542d491536b13b275
snaiffer <snaiffer@gmail.com>
2014-10-22 23:22:21 +0400

    cmd load_text was added

--- cgi.ru/cgi-bin/treemind.py
+++ cgi.ru/cgi-bin/treemind.py
@@ -29,43 +29,44 @@ def for_test():
   except t.TreeException:  
     print("Error: TreeException has occured")
 
-
-import cgi
-for_test()
-
-form = cgi.FieldStorage()
-id = form.getvalue('id', general.rootB_id)
-
-print("Content-Type: text/html\n") 
-
-def getDict(branch):
-  dict = {}
-  dict['id'] = branch.id
-  dict['text'] = branch.caption
-  if branch.subbs_id != [] :
-    dict['state'] = {}
-    dict['state']['opened'] = branch.folded ^ True
-    if dict['state']['opened'] == True:
-      dict['children'] = []
-      for cur_subb in branch.subbs:
-        dict['children'].append(getDict(cur_subb))
-    else:
-      dict['children'] = True
-  return dict  
-
 def getList_subbsOf(branch):
+  """ getList of subbranches of "branch" in format for jstree """
+  def getDict(branch):
+    dict = {}
+    dict['id'] = branch.id
+    dict['text'] = branch.caption
+    if branch.subbs_id != [] :
+      dict['state'] = {}
+      dict['state']['opened'] = branch.folded ^ True
+      if dict['state']['opened'] == True:
+        dict['children'] = []
+        for cur_subb in branch.subbs:
+          dict['children'].append(getDict(cur_subb))
+      else:
+        dict['children'] = True
+    return dict  
+
   list = []
   if branch.subbs_id != [] :
     for cur_subb in branch.subbs:
       list.append(getDict(cur_subb))
   return list    
 
+
+import cgi
+for_test()
+
+form = cgi.FieldStorage()
+cmd = form.getvalue('cmd', general.rootB_id)
+id = form.getvalue('id', general.rootB_id)
+
+print("Content-Type: text/html\n") 
 try:
   with t.Tree(treename) as tree:
-    if id == None or id == general.rootB_id:
-      print(json.dumps(getDict(tree.getB(general.rootB_id))))
-    else:  
+    if cmd == "load_subbs":
       print(json.dumps(getList_subbsOf(tree.getB(id))))
+    if cmd == "load_text":
+      print(tree.getB(id).text)
 except b.BranchException:  
   print("Error: BranchException has occured")
 except t.TreeException:  
